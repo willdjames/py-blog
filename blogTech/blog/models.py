63b6c1c318425671695c3666aba8c04c2302a720
William D C Santana <williamdiegocampos@gmail.com>
2021-02-22 22:44:53 -0300

    Renderizando lista do banco de dados

--- blogTech/blog/models.py
+++ blogTech/blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     titulo = models.TextField()
+    autor = models.TextField()
+    conteudo = models.TextField()
+    data_criacao = models.TextField()
+    data_atualizacao = models.TextField()
 
     def __str__(self):
         return self.titulo
--- blogTech/blog/views.py
+++ blogTech/blog/views.py
@@ -5,8 +5,12 @@ from .models import Blog
 
 # Create your views here.
 def index(request):
-    blogs = Blog.objects.all()
-    return render(request, 'blog/index.html', {'blogs': blogs})
+    blogs = list(Blog.objects.order_by('-data_criacao'))
+    blog_mais_recente = blogs[0]
+    del blogs[0]
+    return render(request, 'blog/index.html', {'blogs': blogs, 
+                                               'titulo': 'Meu Blog', 
+                                               'blog_mais_recente': blog_mais_recente})
 
 def detalhe(request, blog_id):
     try:
